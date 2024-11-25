44704cfea6009920d4f81b6e0e8b33abfa21a74c
monellz <rxzhong@outlook.com>
2024-11-25 18:38:49 +0800

    set task limit for fig12

--- fig12/run.sh
+++ fig12/run.sh
@@ -18,8 +18,10 @@ mkdir -p ${LOG_DIR}
 
 if [[ "$HOST" == "yes" ]]; then
 device=a100
+max_tasks=7
 elif [[ "$HOST" == "fuse0" ]]; then
 device=h100
+max_tasks=2
 else
   echo "unknown host: $HOST"
   exit -1
@@ -31,8 +33,15 @@ echo Running for kernel
 source ${PROJECT_DIR}/baseline_env.sh
 for sys in {torch,dynamo,tensorrt,tvm}; do
 for model in {attn,h2o,roco,kf,snapkv,corm,gemma2}; do
+
+while [ "$(squeue -u $(whoami) --noheader | wc -l)" -ge "${max_tasks}" ]; do
+  echo "waiting for a slot to free up... current tasks:" $(squeue -u $(whoami) --noheader | wc -l)
+  sleep 20
+done
+
 echo "Running ${model} on ${sys}"
 $SRUN python ${PROJECT_DIR}/run_kernel.py -m $model -s $sys 2>&1 | tee ${LOG_DIR}/kernel.${device}.${model}.${sys}.log &
+
 done
 done
 
@@ -42,8 +51,16 @@ wait
 sys=our
 source ${PROJECT_DIR}/our_env.sh
 for model in {attn,h2o,roco,kf,snapkv,corm,gemma2}; do
+
+while [ "$(squeue -u $(whoami) --noheader | wc -l)" -ge "${max_tasks}" ]; do
+  echo "waiting for a slot to free up... current tasks:" $(squeue -u $(whoami) --noheader | wc -l)
+  sleep 20
+done
+
+
 echo "Running ${model} on ${sys}"
 $SRUN python ${PROJECT_DIR}/run_kernel.py -m $model -s $sys 2>&1 | tee ${LOG_DIR}/kernel.${device}.${model}.${sys}.log &
+
 done
 
 wait
@@ -54,8 +71,16 @@ echo Running for e2e
 source ${PROJECT_DIR}/baseline_env.sh
 for sys in {torch,dynamo,tensorrt,tvm}; do
 for model in {attn,h2o,roco,kf,snapkv,corm,gemma2}; do
+
+while [ "$(squeue -u $(whoami) --noheader | wc -l)" -ge "${max_tasks}" ]; do
+  echo "waiting for a slot to free up... current tasks:" $(squeue -u $(whoami) --noheader | wc -l)
+  sleep 20
+done
+
+
 echo "Running ${model} on ${sys}"
 $SRUN python ${PROJECT_DIR}/run_e2e.py -p $HOST -m $model -s $sys 2>&1 | tee ${LOG_DIR}/e2e.${device}.${model}.${sys}.log &
+
 done
 done
 
@@ -65,8 +90,16 @@ wait
 sys=our
 source ${PROJECT_DIR}/our_env.sh
 for model in {attn,h2o,roco,kf,snapkv,corm,gemma2}; do
+
+while [ "$(squeue -u $(whoami) --noheader | wc -l)" -ge "${max_tasks}" ]; do
+  echo "waiting for a slot to free up... current tasks:" $(squeue -u $(whoami) --noheader | wc -l)
+  sleep 20
+done
+
+
 echo "Running ${model} on ${sys}"
 $SRUN python ${PROJECT_DIR}/run_e2e.py -p $HOST -m $model -s $sys 2>&1 | tee ${LOG_DIR}/e2e.${device}.${model}.${sys}.log &
+
 done
 
 wait
